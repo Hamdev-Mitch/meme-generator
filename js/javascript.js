131d9cf711771b59e2c55e70fa919375024124ee
Mitchell Hamilton <mitch@hamdev.net>
2019-07-12 10:24:23 +0800

    Program dynamically generated image
    Use JS to dynamically generate an image using the choose image button.

--- js/javascript.js
+++ js/javascript.js
@@ -1,39 +1,83 @@
-// Use the DOM to select the canvas which has an ID of 'c'.
-var c = document.querySelector("#c");
-var d = document.querySelector("#d");
-// Set the context to 2d
-var ctx = c.getContext("2d");
-var ctx2 = d.getContext("2d");
+// This code is taken from the UDACITY HTML Canvas Course (URL: )
+function textChangeListener (evt) {
+  var id = evt.target.id;
+  var text = evt.target.value;
 
-// Constructor Function for Image
-var image = new Image();
+  if (id == "topLineText"){
+    window.topLineText = text;
+  }
+  else{
+    window.bottomLineText = text;
+  }
 
-// Make a function that happens when the image is loaded
-image.onload = function(){
-  // Print a message that the message has loaded.
-  console.log("Loaded image");
-  // draw image in the top left corner that fills the canvas
-  ctx.drawImage(image, 0 , 0, c.width, c.height);
-  // Save image to
-  var savedImage = c.toDataURL();
-  // Open saved image
-  window.open(savedImage);
+  redrawMeme(window.imageSrc, window.topLineText, window.bottomLineText);
 }
 
-image.src = "assets/badLuckBrian.jpeg";
+function redrawMeme(image, topLine, bottomLine) {
+  // Get Canvas2DContext
+  // Use the DOM to select the canvas which has an ID of 'c'.
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext("2d");
+  //My code here
+  if (image != null)
+  // draw image in the top left corner that fills the canvas
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 
+  // Text Attributes
+  ctx.font = "36pt Impact";
+  ctx.textAlign = "center";
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 3;
 
-ctx2.strokeRect(50, 50, 100, 100);
-ctx2.beginPath();
-ctx2.moveTo(75,75);
-ctx2.lineTo(125,75);
-ctx2.lineTo(125,125);
-ctx2.lineTo(75,75);
-ctx2.fill();
-// ctx.stroke();
+  if (topLine != null) {
+    ctx.fillText(topline, canvas.width / 2, 40);
+    ctx.strokeText(topLine, canvas.width / 2, 40);
+  }
 
-// Set up a local web server so as to be able to save the Image
+if (bottomLine != null) {
+  ctx.fillText(bottomLine, canvas.width / 2, canvas.height - 20);
+  ctx.strokeText(bottomLine, canvas.width / 2, canvas.height - 20);
+}
+}
 
-// Canvas Background Colour
-ctx2.fillStyle = "red";
-ctx.fillRect();
+function saveFile(){
+  window.open(document.querySelector('canvas').toDataURL());
+}
+
+function handleFileSelect(evt){
+  var canvasWidth = 500;
+  var canvasHeight = 500;
+  var file = evt.target.files[0];
+
+  var reader = new FileReader();
+       reader.onload = function(fileObject) {
+         var data = fileObject.target.result;
+
+      // Create an image object
+      //Constructor Function for Image
+       var image = new Image();
+      // Make a function that happens when the image is loaded
+       image.onload = function() {
+
+         window.imageSrc = this;
+         redrawMeme(window.imageSrc, null, null);
+       };
+
+       // Set image data to background image.
+        image.src = data;
+        console.log(fileObject.target.result);
+      };
+  reader.readAsDataURL(file);
+    }
+
+    window.topLineText = "";
+    window.bottomLineText = "";
+    var input1 = document.getElementById('topLineText');
+    var input2 = document.getElementById('bottomLineText');
+    input1.oninput = textChangeListener;
+    input2.oninput = textChangeListener;
+    document.getElementById('file').addEventListener('change', handleFileSelect, false);
+    document.getElementById('saveBtn').addEventListener('click', saveFile, false);
+
+    document.getElementById('badLuckBrianBtn').addEventListener('change', handleFileSelect, false);
